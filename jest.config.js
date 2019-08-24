534493d7983e552f5ef5e33671a65624db9dcb81
fong <fong1143@gmail.com>
2019-08-25 00:19:11 +0800

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -141,8 +141,11 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-
+  testMatch: [
+    // '**/__tests__/**/*.(spec|test)?.[jt]s?(x)',
+    '/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
