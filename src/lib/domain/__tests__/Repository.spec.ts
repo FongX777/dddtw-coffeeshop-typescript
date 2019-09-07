fd036b09e95e8aa1e844217a8968d188cea38ee9
fong <fong1143@gmail.com>
2019-09-07 16:48:46 +0800

    Improve Repository

---
+++ src/lib/domain/__tests__/Repository.spec.ts
@@ -0,0 +1,56 @@
+import { Repository } from '../Repository';
+import { EntityId } from '../EntityId';
+import { AggregateRoot } from '../AggregateRoot';
+
+class TestEntityId extends EntityId<string> {}
+interface TestEntityProps {}
+
+class TestAggregateRoot extends AggregateRoot<TestEntityId, TestEntityProps> {}
+
+class TestRepository implements Repository<TestEntityId, TestAggregateRoot> {
+  tests: TestAggregateRoot[];
+  constructor() {
+    this.tests = [];
+  }
+  async exist(id: TestEntityId): Promise<boolean> {
+    return this.tests.some(test => test.id.equals(id));
+  }
+  async getById(id: TestEntityId): Promise<TestAggregateRoot | undefined> {
+    return this.tests.find(test => test.id.equals(id));
+  }
+  async save(entity: TestAggregateRoot): Promise<void> {
+    if (this.exist(entity.id)) {
+      const targetIndex = this.tests.findIndex(test =>
+        test.id.equals(entity.id)
+      );
+      this.tests.splice(targetIndex, 1, entity);
+    } else {
+      this.tests.push(entity);
+    }
+  }
+  async remove(id: TestEntityId): Promise<void> {
+    if (this.exist(id)) {
+      const targetIndex = this.tests.findIndex(test => test.id.equals(id));
+      this.tests.splice(targetIndex, 1);
+    }
+  }
+}
+
+describe('Repository', () => {
+  const idValue = '123456';
+  const testEntityId = new TestEntityId(idValue);
+  const testEntity = new TestAggregateRoot(testEntityId, {});
+  const repo = new TestRepository();
+
+  it('should create successfully', async () => {
+    repo.save(testEntity);
+    expect(repo.tests.length).toBe(1);
+    const foundEntity = await repo.getById(testEntityId);
+    expect(foundEntity).toBe(testEntity);
+    const existed = await repo.exist(testEntityId);
+    expect(existed).toBeTruthy();
+
+    await repo.remove(testEntityId);
+    expect(repo.tests.length).toBe(0);
+  });
+});
