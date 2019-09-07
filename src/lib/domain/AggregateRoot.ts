8fa1f65a77273e62f04fb7440b570c138f62f02e
fong <fong1143@gmail.com>
2019-09-08 02:29:32 +0800

    Update Aggregate root

--- src/lib/domain/AggregateRoot.ts
+++ src/lib/domain/AggregateRoot.ts
@@ -1,40 +1,7 @@
 import { Entity } from './Entity';
 import { EntityId } from './EntityId';
-import { DomainEvent } from './event/DomainEvent';
-import { domainEventsHandler } from './event/domainEventsHandler';
 
 export abstract class AggregateRoot<
   Id extends EntityId<unknown>,
   Props
-> extends Entity<Id, Props> {
-  private _domainEvents: DomainEvent[] = [];
-
-  get domainEvents(): DomainEvent[] {
-    return this._domainEvents;
-  }
-
-  protected addDomainEvent(domainEvent: DomainEvent): void {
-    // Add the domain event to this aggregate's list of domain events
-    this._domainEvents.push(domainEvent);
-    // Add this aggregate instance to the domain event's list of aggregates who's
-    // events it eventually needs to dispatch.
-    domainEventsHandler.markAggregateForDispatch(this);
-    // Log the domain event
-    this.logDomainEventAdded(domainEvent);
-  }
-
-  clearEvents(): void {
-    this._domainEvents.splice(0, this._domainEvents.length);
-  }
-
-  private logDomainEventAdded(domainEvent: DomainEvent): void {
-    const thisClass = Reflect.getPrototypeOf(this);
-    const domainEventClass = Reflect.getPrototypeOf(domainEvent);
-    console.info(
-      `[Domain Event Created]:`,
-      thisClass.constructor.name,
-      '==>',
-      domainEventClass.constructor.name
-    );
-  }
-}
+> extends Entity<Id, Props> {}
--- src/lib/domain/__tests__/AggregateRoot.spec.ts
+++ src/lib/domain/__tests__/AggregateRoot.spec.ts
@@ -1,36 +1,15 @@
 import { EntityId } from '../EntityId';
-import { DomainEvent } from '../event/DomainEvent';
 import { AggregateRoot } from '../AggregateRoot';
-
-class CreateEvent implements DomainEvent {
-  occuredDate: Date;
-  aggregateId: EntityId<unknown>;
-  constructor(aggregateId: EntityId<unknown>) {
-    this.occuredDate = new Date();
-    this.aggregateId = aggregateId;
-  }
-}
 
 class TestEntityId extends EntityId<string> {}
 interface TestEntityProps {}
-class TestAggregateRoot extends AggregateRoot<TestEntityId, TestEntityProps> {
-  static create(): TestAggregateRoot {
-    const id = new TestEntityId('12345677');
-    const testAggregateRoot = new TestAggregateRoot(id, {});
-    testAggregateRoot.addDomainEvent(new CreateEvent(id));
-    return testAggregateRoot;
-  }
-}
+class TestAggregateRoot extends AggregateRoot<TestEntityId, TestEntityProps> {}
 
 describe('Aggregate Root', () => {
-  const testEntityId = new TestEntityId('123456');
-  const testAggregateRoot = new TestAggregateRoot(testEntityId, {});
-  const ag = TestAggregateRoot.create();
-  it('should have domain events', () => {
-    expect(testAggregateRoot.domainEvents.length).toBe(0);
-  });
+  it('should be created', () => {
+    const testEntityId = new TestEntityId('123456');
+    const testAggregateRoot = new TestAggregateRoot(testEntityId, {});
 
-  it('should have one domain events', () => {
-    expect(ag.domainEvents.length).toBe(1);
+    expect(testAggregateRoot.id.equals(testEntityId)).toBeTruthy();
   });
 });
