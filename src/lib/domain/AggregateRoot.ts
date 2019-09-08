55c2bb35d69c6b334446cab63fec49223676fb60
fong <fong1143@gmail.com>
2019-09-09 00:22:21 +0800

    Add domain events to aggregate

--- src/lib/domain/AggregateRoot.ts
+++ src/lib/domain/AggregateRoot.ts
@@ -1,7 +1,26 @@
 import { Entity } from './Entity';
 import { EntityId } from './EntityId';
+import { DomainEvent } from './event/DomainEvent';
 
 export abstract class AggregateRoot<
   Id extends EntityId<unknown>,
   Props
-> extends Entity<Id, Props> {}
+> extends Entity<Id, Props> {
+  private _domainEvents: DomainEvent[] = [];
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents;
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent);
+  }
+
+  protected addDomainEvents(domainEvents: DomainEvent[]): void {
+    this._domainEvents.push(...domainEvents);
+  }
+
+  clearEvents(): void {
+    this._domainEvents = [];
+  }
+}
