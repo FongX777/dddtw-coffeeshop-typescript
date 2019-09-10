7ec30e6821be648ce2373b0f2a5a881936ce308c
fong <fong1143@gmail.com>
2019-09-11 03:00:40 +0800

    Fix npm ci 2

--- src/inventory/domain/inventory/InventoryRepository.ts
+++
@@ -1,5 +0,0 @@
-import { InventoryId, Inventory } from './Inventory';
-import { Repository } from '../../../lib/domain';
-
-export interface InventoryRepository
-  extends Repository<InventoryId, Inventory> {}
